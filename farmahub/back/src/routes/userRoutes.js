dc2a04ae05f08b083cf080c61fbd65ead3a42dfa
Tatiane Feitosa <tatianegomes2312@gmail.com>
2025-11-06 17:22:28 -0300

    Estrutura back-end alterada

--- farmahub/back/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { UserController } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/', UserController.listar);
-router.post('/', UserController.criar);
-
-export default router;
